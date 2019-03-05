62aca0dffaa95418d44232d9d89ab583a1166ba0
Rahim Ismaili <ismailirahim6@gmail.com>
2019-03-05 14:42:42 +0100

    javascript1 homework  creating a future age calculator

---
+++ javascript/javascript1/week1/main.js
@@ -0,0 +1,8 @@
+//Age-ify (A future age calculator)
+
+var yearOfBirth = 1988;
+var yearFuture = 2038;
+var age;
+age =  yearFuture - yearOfBirth;
+
+console.log ("You will be " + age + " years old in " + yearFuture);
